582ddd44d91d06756219a3a371e0cc7ae71c148c
svetlanaLevchuk <svetlana-levchuk@tut.by>
2019-03-06 17:33:25 +0300

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,11 +10,15 @@ public class Application {
     public static void main (String args[]) {
         Company belavia = init();
         System.out.println("Lets start.\n");
+        //для интереса можешь почитать про String.format - позволяет удобно строки составлять с разными параметрами. тут не критично,
+        //потому что ты одно число в строку вставляешь, но если было бы 2 и больше, то со String.format было бы удобнее
         System.out.println("Total people capacity of company: " + belavia.countPeopleCapacity());
         System.out.println("Total carrying capacity of company: " + belavia.countCarryingCapacity());
         belavia.sortByDistance();
     }
 
+    //попробуй вынести в отдельный класс - CompanyBuilder или Factory - тут хорошо подойдет один из этих паттернов
+    //а следующим шагом запилим вычитку данных из экселя или хмл - будет аналог БД - и уберем хардкод
     public static Company init() {
         Company company = new Company("Belavia");
         TransportPlane transportFirst = new TransportPlane(Consignment.CAR, 26, 5000);
